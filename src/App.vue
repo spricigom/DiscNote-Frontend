ba4fe02ad574fc09f282ba331e6ceba05149e3ed
LuizEduardoCR <luizeduardocabraldarosa@gmail.com>
2025-06-07 22:57:33 -0300

    feat: carrossel com avaliacoes em baixo finalizado

--- src/App.vue
+++ src/App.vue
@@ -21,31 +21,55 @@ import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
       <Slide>
         <div class="carousel-slide">
           <img src="#" alt="" class="sliderImage">
+          <div class="avaliacao">
+            <div class="av1"><p><i class="pi pi-clipboard"></i>666k</p></div>
+            <div class="av2"><p><i class="pi pi-star"></i>4.7/5</p></div>
+          </div>
         </div>
       </Slide>
       <Slide>
         <div class="carousel-slide">
           <img src="#" alt="" class="sliderImage">
+          <div class="avaliacao">
+            <div class="av1"><p><i class="pi pi-clipboard"></i>666k</p></div>
+            <div class="av2"><p><i class="pi pi-star"></i>4.7/5</p></div>
+          </div>
         </div>
       </Slide>
       <Slide>
         <div class="carousel-slide">
           <img src="#" alt="" class="sliderImage">
+          <div class="avaliacao">
+            <div class="av1"><p><i class="pi pi-clipboard"></i>666k</p></div>
+            <div class="av2"><p><i class="pi pi-star"></i>4.7/5</p></div>
+          </div>
         </div>
       </Slide>
       <Slide>
         <div class="carousel-slide">
           <img src="#" alt="" class="sliderImage">
+          <div class="avaliacao">
+            <div class="av1"><p><i class="pi pi-clipboard"></i>666k</p></div>
+            <div class="av2"><p><i class="pi pi-star"></i>4.7/5</p></div>
+          </div>
         </div>
       </Slide>
       <Slide>
         <div class="carousel-slide">
           <img src="#" alt="" class="sliderImage">
+          <div class="avaliacao">
+            <div class="av1"><p><i class="pi pi-clipboard"></i>666k</p></div>
+            <div class="av2"><p><i class="pi pi-star"></i>4.7/5</p></div>
+          </div>
         </div>
       </Slide>
       <Slide>
         <div class="carousel-slide">
           <img src="#" alt="" class="sliderImage">
+          <div class="avaliacao">
+            <div class="av1"><p><i class="pi pi-clipboard"></i>666k</p></div>
+            <div class="av2"><p><i class="pi pi-star"></i>4.7/5</p></div>
+          </div>
         </div>
       </Slide>
 
@@ -107,20 +131,58 @@ main {
 .divisao hr {
   width: 100%;
   border: #145d91 1px solid;
+  margin-bottom: 2%;
 }
 .carousel-custom{
-  width: 70vw;
+  width: 70%;
   height: 35vh;
   margin: 0 0.2vw;
 }
 .carousel-slide{
   width: 85%;
+  height: 100%;
+}
+.sliderImage{
+  object-fit: cover;
+  width: 100%;
   height: 85%;
-  background-color: #0c1414 ;
   border-radius: 2vw;
   transition: .1s;
+  background-color: rgba(0, 0, 0, 0.397);
 }
-.carousel-slide:hover{
+.avaliacao{
+  height: 15%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10%;
+}
+.avaliacao p{
+  color: white;
+  font-family: 'DM Mono', sans-serif;
+}
+.avaliacao i{
+  color: #145d91;
+  font-size: 1.2rem;
+  margin-right: 0.4rem;
+  position: relative;
+  top: .2vh;
+}
+.av1{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 100%;
+}
+.av2{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 100%;
+
+}
+.sliderImage:hover{
   border: #ecc415 3px solid;
 }
 .carousel-custom :deep(.carousel__prev), 
@@ -139,5 +201,6 @@ main {
 .carousel-custom :deep(.carousel__next) {
   right: -5%;
 }
+
 
 </style>
