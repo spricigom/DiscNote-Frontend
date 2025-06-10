b890bd481b9bc4a97d0555687c34a1db4309b92a
LuizEduardoCR <luizeduardocabraldarosa@gmail.com>
2025-06-10 09:29:53 -0300

    feat: parte de resenhas iniciada

--- src/App.vue
+++ src/App.vue
@@ -24,18 +24,95 @@ import Slider from '@/components/Slider.vue'
       <hr>
     </div>
     <Slider />
-    
+
     <div class="divisao">
       <h3>Resenhas</h3>
       <hr>
     </div>
-    
+    <div class="resenha">
+      <div class="foto-resenha">
+        <img src="#" alt="">
+      </div>
+
+      <div class="corpo">
+        <div class="titulo">
+          <h1>Nome da Música</h1>
+          <h4>nome do artista</h4>
+        </div>
+
+        <div class="username">
+          <div class="foto-username">
+            <img src="#" alt="">
+          </div>
+          <p>@username</p>
+        </div>
+
+
+
+      </div>
+
+    </div>
+
   </main>
 
   <RouterView />
 </template>
 
 <style scoped>
+.corpo{
+  height: 100%;
+}
+.foto-username{
+  background-color: rgba(0, 0, 0, 0.315);
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+}
+.foto-username img{
+  object-fit: cover;
+}
+.username p{
+  font-family: 'Archivo', sans-serif;
+  color: #c4c4c4;
+  font-weight: 500;
+}
+.username{
+  display: flex;
+  flex-direction:row;
+  margin-top: 4vh;
+  align-items: center;
+  gap: 3%;
+}
+.titulo h4{
+  font-family: 'Archivo', sans-serif;
+  color: white;
+  font-weight: 400;
+}
+.titulo h1 {
+  font-family: 'Montserrat', sans-serif;
+  color: white;
+  font-weight: 600;
+}
+
+.foto-resenha {
+  background-color: rgba(0, 0, 0, 0.308);
+  width: 250px;
+  height: 250px;
+  border-radius: 2vw;
+  display: flex;
+
+}
+
+.resenha {
+  width: 67%;
+  height: 50vh;
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 10vh;
+  gap: 2vw;
+}
+
+
 main {
   display: flex;
   flex-direction: column;
