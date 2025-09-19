b14e587e7c96b27675a87a8f2e527a680f4423fc
LuizEduardoCR <luizeduardocabraldarosa@gmail.com>
2025-09-18 23:52:57 -0300

    feat: landing page finalizada

--- src/views/LandingPageView.vue
+++ src/views/LandingPageView.vue
@@ -2,21 +2,13 @@
 import { ref } from 'vue'
 // Nenhuma lógica ainda, só layout.
 // Depois você pode integrar rotas ou props aqui.
+import HeaderLandingPage from '@/components/HeaderLandingPage.vue'
 </script>
 
 <template>
   <div class="landing">
     <!-- Header -->
-    <header>
-      <div class="logo">
-        Disc<span class="note">♩</span>note
-      </div>
-      <nav>
-        <a href="#">Login</a>
-        <a href="#">Como funciona?</a>
-        <a href="#" class="btn-outline">Quem Somos?</a>
-      </nav>
-    </header>
+<HeaderLandingPage />
 
     <!-- Conteúdo central -->
     <main>
@@ -25,7 +17,8 @@ import { ref } from 'vue'
         Descubra novos artistas <br />
         Compartilhe playlists com o mundo
       </h1>
-      <a href="#" class="btn-primary">Cadastre-se</a>
+      <RouterLink to="/cadastro"><button class="btn-primary"><p>Cadastre-se</p></button></RouterLink>
+
     </main>
   </div>
 </template>
@@ -47,7 +40,21 @@ import { ref } from 'vue'
 
   /* Fundo aplicado na página inteira */
 }
+main a{
+  z-index: 1;
+}
 
+.landing::before{
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(to bottom, rgba(0,0,0,0.6), rgba(0,0,0,0));
+  z-index: 0;
+  pointer-events: none;
+}
 /* Header */
 header {
   width: 100%;
@@ -111,26 +118,36 @@ main {
   padding: 0 20px;
 background: linear-gradient(to bottom, rgba(0,0,0,0.8), rgba(0,0,0,0.4)),
               url('@/assets/fundo2.jpeg') no-repeat center center/cover;
-  background-size: 145%;
+  background-size: 137%;
 }
 
 main h1 {
-  font-size: 28px;
-  margin-bottom: 20px;
-  line-height: 1.5;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 500;
+  z-index: 1;
+  margin-top: 30vh;
 }
 
 .btn-primary {
-  background: #1e90ff;
+  background: #145d91;
   color: white;
-  padding: 12px 30px;
-  border-radius: 6px;
+  padding: 1.5vh 2.5vw;
+  border: none;
+  border-radius: 12px;
   text-decoration: none;
-  font-size: 16px;
   transition: 0.3s;
+  margin-top: 5vh;
 }
 
 .btn-primary:hover {
   background: #187bcd;
+}
+
+.btn-primary p{
+  font-family: 'Poppins', sans-serif;
+  font-weight: 500;
+  font-size: 1.2rem;
+  color: white;
+
 }
 </style>
