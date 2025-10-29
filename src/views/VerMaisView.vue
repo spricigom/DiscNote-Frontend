586801b6d09d8317c6f91ad6144ef0f87c7eb922
LuizEduardoCR <luizeduardocabraldarosa@gmail.com>
2025-10-29 15:14:41 -0300

    feat: correções

--- src/views/VerMaisView.vue
+++ src/views/VerMaisView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { computed, reactive } from 'vue'
 import Header from '@/components/HeaderComp.vue'
-import Footer  from '@/components/Footer.vue'
+import Footer from '@/components/Footer.vue'
 import { useAuthStore } from '@/stores/auth'
 
 const authStore = useAuthStore()
@@ -52,9 +52,7 @@ const authStore = useAuthStore()
       <div class="direita">
         <div class="user">
           <img src="" alt="" />
-          <p>
-            Resenha escrita por <span>@username</span>
-          </p>
+          <p>Resenha escrita por <span>@username</span></p>
         </div>
         <hr />
         <div class="area-titulos">
@@ -62,11 +60,14 @@ const authStore = useAuthStore()
           <h2 class="artista">Nome do Artista</h2>
         </div>
 
-
         <div class="info">
           <div class="stat rating">
-            <div class="stars" >
-              <i class="pi pi-star-fill" style="color: #ecc815;"></i><i class="pi pi-star-fill" style="color: #ecc815;"></i><i class="pi pi-star-fill" style="color: #ecc815;"></i><i class="pi pi-star-fill" style="color: #ecc815;"></i><i class="pi pi-star-fill" style="color: #ecc815;"></i>
+            <div class="stars">
+              <i class="pi pi-star-fill" style="color: #ecc815"></i
+              ><i class="pi pi-star-fill" style="color: #ecc815"></i
+              ><i class="pi pi-star-fill" style="color: #ecc815"></i
+              ><i class="pi pi-star-fill" style="color: #ecc815"></i
+              ><i class="pi pi-star-fill" style="color: #ecc815"></i>
               <!-- <span class="avg">{{ stats.average.toFixed(1) }}</span> -->
             </div>
           </div>
@@ -74,14 +75,39 @@ const authStore = useAuthStore()
             <i class="pi pi-heart-fill"></i>
           </div>
 
-            <div class="data">
-              <p>29/11/2024</p>
-            </div>
+          <div class="data">
+            <p>29/11/2024</p>
+          </div>
         </div>
 
         <div class="texto-resenha">
           <p>
-           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis viverra diam quis leo finibus, vel posuere augue volutpat. Aenean orci lacus, luctus sed faucibus vel, ultrices ut leo. Quisque cursus libero eros, eget fringilla mi convallis a. Nulla porttitor lorem ac diam mollis scelerisque. Praesent ut luctus tellus, id facilisis felis. Donec sit amet arcu vehicula, lacinia eros et, vestibulum risus. Aenean aliquet malesuada tristique. Ut porta diam nisl, eu posuere lorem sodales vel. Curabitur dignissim accumsan volutpat. Integer vitae neque non erat iaculis laoreet. Curabitur auctor vel nisl sed bibendum. Nunc ipsum leo, pharetra tempor leo eget, tincidunt placerat libero. Pellentesque non tortor velit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis viverra diam quis leo finibus, vel posuere augue volutpat. Aenean orci lacus, luctus sed faucibus vel, ultrices ut leo. Quisque cursus libero eros, eget fringilla mi convallis a. Nulla porttitor lorem ac diam mollis scelerisque. Praesent ut luctus tellus, id facilisis felis. Donec sit amet arcu vehicula, lacinia eros et, vestibulum risus. Aenean aliquet malesuada tristique. Ut porta diam nisl, eu posuere lorem sodales vel. Curabitur dignissim accumsan volutpat. Integer vitae neque non erat iaculis laoreet. Curabitur auctor vel nisl sed bibendum. Nunc ipsum leo, pharetra tempor leo eget, tincidunt placerat libero. Pellentesque non tortor velit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis viverra diam quis leo finibus, vel posuere augue volutpat. Aenean orci lacus, luctus sed faucibus vel, ultrices ut leo. Quisque cursus libero eros, eget fringilla mi convallis a. Nulla porttitor lorem ac diam mollis scelerisque. Praesent ut luctus tellus, id facilisis felis. Donec sit amet arcu vehicula, lacinia eros et, vestibulum risus. Aenean aliquet malesuada tristique. Ut porta diam nisl, eu posuere lorem sodales vel. Curabitur dignissim accumsan volutpat. Integer vitae neque non erat iaculis laoreet. Curabitur auctor vel nisl sed bibendum. Nunc ipsum leo, pharetra tempor leo eget, tincidunt placerat libero. Pellentesque non tortor velit.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis viverra diam quis leo
+            finibus, vel posuere augue volutpat. Aenean orci lacus, luctus sed faucibus vel,
+            ultrices ut leo. Quisque cursus libero eros, eget fringilla mi convallis a. Nulla
+            porttitor lorem ac diam mollis scelerisque. Praesent ut luctus tellus, id facilisis
+            felis. Donec sit amet arcu vehicula, lacinia eros et, vestibulum risus. Aenean aliquet
+            malesuada tristique. Ut porta diam nisl, eu posuere lorem sodales vel. Curabitur
+            dignissim accumsan volutpat. Integer vitae neque non erat iaculis laoreet. Curabitur
+            auctor vel nisl sed bibendum. Nunc ipsum leo, pharetra tempor leo eget, tincidunt
+            placerat libero. Pellentesque non tortor velit. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Duis viverra diam quis leo finibus, vel posuere augue volutpat. Aenean
+            orci lacus, luctus sed faucibus vel, ultrices ut leo. Quisque cursus libero eros, eget
+            fringilla mi convallis a. Nulla porttitor lorem ac diam mollis scelerisque. Praesent ut
+            luctus tellus, id facilisis felis. Donec sit amet arcu vehicula, lacinia eros et,
+            vestibulum risus. Aenean aliquet malesuada tristique. Ut porta diam nisl, eu posuere
+            lorem sodales vel. Curabitur dignissim accumsan volutpat. Integer vitae neque non erat
+            iaculis laoreet. Curabitur auctor vel nisl sed bibendum. Nunc ipsum leo, pharetra tempor
+            leo eget, tincidunt placerat libero. Pellentesque non tortor velit. Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit. Duis viverra diam quis leo finibus, vel posuere
+            augue volutpat. Aenean orci lacus, luctus sed faucibus vel, ultrices ut leo. Quisque
+            cursus libero eros, eget fringilla mi convallis a. Nulla porttitor lorem ac diam mollis
+            scelerisque. Praesent ut luctus tellus, id facilisis felis. Donec sit amet arcu
+            vehicula, lacinia eros et, vestibulum risus. Aenean aliquet malesuada tristique. Ut
+            porta diam nisl, eu posuere lorem sodales vel. Curabitur dignissim accumsan volutpat.
+            Integer vitae neque non erat iaculis laoreet. Curabitur auctor vel nisl sed bibendum.
+            Nunc ipsum leo, pharetra tempor leo eget, tincidunt placerat libero. Pellentesque non
+            tortor velit.
           </p>
         </div>
         <div class="curtida-e-comentario">
@@ -89,7 +115,6 @@ const authStore = useAuthStore()
             <i class="pi pi-thumbs-up"></i>
             <p>2.500 curtidas</p>
           </div>
-
         </div>
       </div>
     </div>
@@ -188,47 +213,64 @@ main {
   display: flex;
   flex-direction: row;
 }
-.rating span{
+.rating span {
   font-family: 'Archivo', sans-serif;
   font-size: 1.2vw;
 }
 .rating .stars {
-  font-size: 1.5vw;
+  font-size: 1.1vw;
   color: #ecc815;
-   display: flex;
+  display: flex;
   flex-direction: row;
   align-items: center;
-  gap: .5vw;
+  gap: 0.5vw;
 }
 
-
-
-.info{
+.info {
   display: flex;
   flex-direction: row;
   align-items: center;
   gap: 1.7vw;
   margin-top: 2.5vh;
 }
-.info i{
+.info i {
   color: #145d91;
   font-size: 1.8vw;
-
 }
-.data p{
+.data p {
   font-family: 'Archivo', sans-serif;
   font-size: 1.1vw;
   color: #9e9e9e;
 }
 
-.texto-resenha { width: 100%; margin-top: 2vh; }
+.texto-resenha {
+  width: 100%;
+  margin-top: 2vh;
+}
 .texto-resenha p {
   font-family: 'Archivo', sans-serif;
+  font-size: 1.1vw;
+  font-weight: 400;
+  letter-spacing: 1px;
   color: white;
 }
 
-.curtida-e-comentario { display:flex; gap:2vw; margin-top:4vh; }
-.curtida { display:flex; align-items:center; gap:.5vw; }
-.curtida i { color:#145d91; font-size:1.4vw; }
-.curtida p { font-family:'DM Mono', sans-serif; color:white; }
+.curtida-e-comentario {
+  display: flex;
+  gap: 2vw;
+  margin-top: 4vh;
+}
+.curtida {
+  display: flex;
+  align-items: center;
+  gap: 0.5vw;
+}
+.curtida i {
+  color: #145d91;
+  font-size: 1.4vw;
+}
+.curtida p {
+  font-family: 'DM Mono', sans-serif;
+  color: white;
+}
 </style>
